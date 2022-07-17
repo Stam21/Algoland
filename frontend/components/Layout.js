3c2860063f6043b626994111562c0f8fc0332b60
GeorgeMoucht <giorgosmoucht@gmail.com>
2022-07-18 00:44:06 +0300

    simple routing

---
+++ frontend/components/Layout.js
@@ -0,0 +1,10 @@
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+export default function Layout() {
+    return (
+        <Navbar />
+        // Here Footer also will be placed because
+        // both of this components are static renderic
+    )
+}
--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -2,15 +2,15 @@ import Head from 'next/head'
 import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function About() {
   return (
       <div className={styles.container}>
         <Head>
-          <title>Algoland - Home</title>
+          <title>Algoland - About</title>
           <link rel='icon' href='/favicon.ico'/>
         </Head>
         <Layout />
-        <h1>Home</h1>
+        <h1>About</h1> 
       </div>
   )
 }

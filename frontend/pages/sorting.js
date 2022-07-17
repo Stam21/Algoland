4ed01ce4442003ef244ac3b0744d8252e5c4eac5
GeorgeMoucht <giorgosmoucht@gmail.com>
2022-07-18 02:35:39 +0300

    fix-merge-errors

--- frontend/pages/sorting.js
+++ frontend/pages/sorting.js
@@ -1,14 +1,11 @@
 
 import Layout from '../components/Layout'
 import BubbleSort from '../components/BubbleSort'
-import styles from '../styles/Home.module.css'
 
 export default function Sorting() {
   const numbers = [1,2,3,4345,23,523,65,235,764]
   return (
-      <div className={styles.container}>
-
-        <Layout />
+      <div>
         <h1>Sorting</h1>
         <BubbleSort items={numbers} /> 
       </div>
